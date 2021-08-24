f5a7f3c4721333211f078d6821974cdaf2cce157
FerroEduardo <47820549+ferroeduardo@users.noreply.github.com>
2021-08-24 19:33:27 -0300

    GuildInfo new method to sendMessage(JDA,Message)

--- src/main/java/br/ufrrj/dcc/entity/GuildInfo.java
+++ src/main/java/br/ufrrj/dcc/entity/GuildInfo.java
@@ -1,6 +1,13 @@
 package br.ufrrj.dcc.entity;
 
+import br.ufrrj.dcc.exceptions.GuildNotFoundException;
+import br.ufrrj.dcc.exceptions.TextChannelNotFoundExists;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+
 import javax.persistence.*;
+import java.util.Optional;
 
 @Entity
 @Table(name = "guilds")
@@ -52,6 +59,24 @@ public class GuildInfo {
 
     public void setGuildChannelId(long guildChannelId) {
         this.guildChannelId = guildChannelId;
+    }
+
+    public void sendMessage(JDA jda, String message) {
+        try {
+            Optional<Guild> dccGuild = Optional.ofNullable(jda.getGuildById(guildId));
+            Optional<TextChannel> textChannel = Optional.ofNullable(dccGuild.orElseThrow(() ->
+                    new GuildNotFoundException(String.format("Guild '%d' not found", guildId))
+            ).getTextChannelById(guildChannelId));
+            textChannel.orElseThrow(() ->
+                    new TextChannelNotFoundExists(String.format("TextChannel '%d' not found in Guild '%d'", guildChannelId, guildId))
+            ).sendMessage(message).queue();
+        } catch (GuildNotFoundException e) {
+            e.printStackTrace();
+            System.out.printf("Failed to send message to Guild '%d' because this Guild does not exists anymore%n", guildId);
+        } catch (TextChannelNotFoundExists e) {
+            e.printStackTrace();
+            System.out.printf("Failed to send message to Guild '%d' and TextChannel '%d' because this TextChannel does not exists anymore%n", guildId, guildChannelId);
+        }
     }
 
     @Override
