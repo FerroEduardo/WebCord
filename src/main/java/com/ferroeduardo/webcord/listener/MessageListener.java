11b4d35a285347d0e4cfe1887ec8b694264b096a
FerroEduardo <47820549+ferroeduardo@users.noreply.github.com>
2021-09-01 17:19:40 -0300

    Now the bot also supports private messages

--- src/main/java/com/ferroeduardo/webcord/listener/MessageListener.java
+++ src/main/java/com/ferroeduardo/webcord/listener/MessageListener.java
@@ -1,11 +1,12 @@
 package com.ferroeduardo.webcord.listener;
 
+import com.ferroeduardo.webcord.Util;
 import com.ferroeduardo.webcord.entity.GuildInfo;
 import com.ferroeduardo.webcord.exception.AlreadyExistsException;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -38,14 +39,15 @@ public class MessageListener extends ListenerAdapter {
         this.webObservers = webObservers;
     }
 
+
     @Override
-    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
-        if (event.getAuthor().isBot())
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) {
             return;
-        Member member = event.getMember();
+        }
         Message msg = event.getMessage();
-        Guild guild = event.getGuild();
-        TextChannel channel = event.getChannel();
+        ChannelType channelType = event.getChannelType();
+        MessageChannel messageChannel = event.getChannel();
         User author = event.getAuthor();
         String content = msg.getContentRaw();
 
@@ -56,7 +58,7 @@ public class MessageListener extends ListenerAdapter {
 
         if (content.equals(COMMAND_PREFIX + "ping")) {
             long time = System.currentTimeMillis();
-            channel.sendMessage("Pong!")
+            messageChannel.sendMessage("Pong!")
                     .queue(response -> {
                         response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
                     });
@@ -67,87 +69,21 @@ public class MessageListener extends ListenerAdapter {
                                             "Ocupado - Algum serviço está fora do ar\n" +
                                             "Ausente - Inicializando bot\n" +
                                             "Invisível - Estou fora do ar\n\n");
-            descriptionStringBuilder.append("dcc.help - Comandos\n");
             descriptionStringBuilder.append("dcc.ping - Ping\n");
+            descriptionStringBuilder.append("dcc.help - Comandos\n");
+            descriptionStringBuilder.append("dcc.invite - Convite do bot");
             descriptionStringBuilder.append("dcc.status - Estado atual dos sites cadastrados\n");
+            descriptionStringBuilder.append("Somente servidores---------------------------------------\n");
             descriptionStringBuilder.append("dcc.add - Adiciona canal atual para receber avisos\n");
             descriptionStringBuilder.append("dcc.remove - Remove canal atual e deixa de receber avisos\n");
-            descriptionStringBuilder.append("dcc.invite - Convite do bot");
             EmbedBuilder eb = new EmbedBuilder();
             eb.setTimestamp(event.getMessage().getTimeCreated());
             eb.setColor(new Color((int) (Math.random() * 0x1000000)));
-            eb.setTitle("Comandos");
+            eb.setTitle("Ajuda");
             eb.setDescription(descriptionStringBuilder.toString());
             msg.reply(eb.build()).queue();
-        } else if (content.equals(COMMAND_PREFIX + "add")) {
-            Optional<Role> optionalRole = member.getRoles().stream().filter(role -> role.hasPermission(Permission.ADMINISTRATOR)).findFirst();
-            boolean isAdmin = optionalRole.isPresent();
-            if (isAdmin) {
-                long guildId = guild.getIdLong();
-                long channelId = channel.getIdLong();
-                EntityManager entityManager = factory.createEntityManager();
-                try {
-                    GuildInfo singleResult = entityManager
-                            .createQuery("SELECT g FROM GuildInfo AS g WHERE g.guildId=?1 and g.guildChannelId=?2", GuildInfo.class)
-                            .setParameter(1, guildId)
-                            .setParameter(2, channelId)
-                            .setMaxResults(1)
-                            .getSingleResult();
-                    if (singleResult != null) {
-                        throw LOGGER.throwing(new AlreadyExistsException("Canal já foi adicionado anteriormente"));
-                    }
-                } catch (NoResultException e) {
-                    LOGGER.trace(e);
-                    GuildInfo guildInfo = new GuildInfo(guildId, channelId);
-                    entityManager.getTransaction().begin();
-                    entityManager.persist(guildInfo);
-                    entityManager.getTransaction().commit();
-                    entityManager.close();
-                    msg.reply("Configurado com sucesso").queue(deleteMessagesAfterTime);
-                } catch (AlreadyExistsException e) {
-                    LOGGER.trace(e);
-                    msg.reply(e.getMessage()).queue(deleteMessagesAfterTime);
-                } catch (Exception e) {
-                    LOGGER.trace(e);
-                    msg.reply("Falha ao adicionar o canal").queue(deleteMessagesAfterTime);
-                }
-            } else {
-                msg.reply("Você não é um administrador para realizar essa ação").queue(deleteMessagesAfterTime);
-            }
-        } else if (content.equals(COMMAND_PREFIX + "remove")) {
-            boolean isAdmin = member.getRoles().stream().anyMatch(role -> role.hasPermission(Permission.ADMINISTRATOR));
-            if (isAdmin) {
-                long guildId = guild.getIdLong();
-                long channelId = channel.getIdLong();
-                EntityManager entityManager = factory.createEntityManager();
-                try {
-                    GuildInfo singleResult = entityManager
-                            .createQuery("SELECT g FROM GuildInfo AS g WHERE g.guildId=?1 and g.guildChannelId=?2", GuildInfo.class)
-                            .setParameter(1, guildId)
-                            .setParameter(2, channelId)
-                            .setMaxResults(1)
-                            .getSingleResult();
-
-                    entityManager.getTransaction().begin();
-                    entityManager.remove(singleResult);
-                    entityManager.getTransaction().commit();
-                    entityManager.close();
-                    msg.reply("Canal removido com sucesso").queue(deleteMessagesAfterTime);
-                } catch (NoResultException e) {
-                    LOGGER.trace(e);
-                    msg.reply("Parece que esse canal não está cadastrado").queue(deleteMessagesAfterTime);
-                } catch (Exception e) {
-                    LOGGER.trace(e);
-                    msg.reply("Falha ao remover o canal").queue(deleteMessagesAfterTime);
-                }
-            } else {
-                msg.reply("Você não é um administrador para realizar essa ação").queue(deleteMessagesAfterTime);
-            }
         } else if (content.equals(COMMAND_PREFIX + "invite")) {
-            long clientId = event.getJDA().getSelfUser().getIdLong();
-            int permission = 2048;
-            String link = String.format("https://discord.com/api/oauth2/authorize?client_id=%d&permissions=%d", clientId, permission) + "&scope=bot%20applications.commands";
-            msg.reply("Convite: " + link).queue();
+            msg.reply("Convite: " + Util.getInviteLink(event.getJDA())).queue();
         } else if (content.equals(COMMAND_PREFIX + "status")) {
             if (webObservers == null) {
                 msg.reply("Aguarde um momento...").queue(deleteMessagesAfterTime);
@@ -161,20 +97,20 @@ public class MessageListener extends ListenerAdapter {
                         if (currentWebsiteStatus == WebsiteStatus.TIMEOUT) {
                             websiteStatusStringBuilder.append(String.format(
                                     "%s:\n" +
-                                    "- Status: %s\n" +
-                                    "- Quantidade de Timeouts: %d\n" +
-                                    "- Desde: %s\n\n",
+                                            "- Status: %s\n" +
+                                            "- Quantidade de Timeouts: %d\n" +
+                                            "- Desde: %s\n\n",
                                     name, currentWebsiteStatus.name(), webObserver.getTimeoutCount(), latestStatusTime.format(formatter)));
                         } else {
                             websiteStatusStringBuilder.append(String.format(
                                     "%s:\n" +
-                                    "- Status: %s\n\n",
+                                            "- Status: %s\n\n",
                                     name, currentWebsiteStatus.name()));
                         }
                     } else {
                         websiteStatusStringBuilder.append(String.format(
                                 "%s:\n" +
-                                "- Status: AGUARDE\n\n",
+                                        "- Status: AGUARDE\n\n",
                                 name));
                     }
                 });
@@ -184,6 +120,77 @@ public class MessageListener extends ListenerAdapter {
                 eb.setTitle("Site - Status");
                 eb.setDescription(websiteStatusStringBuilder.toString());
                 msg.reply(eb.build()).queue();
+            }
+        }
+
+        if (channelType == ChannelType.TEXT) {
+            TextChannel textChannel = event.getTextChannel();
+            Member member = event.getMember();
+            Guild guild = event.getGuild();
+            if (content.equals(COMMAND_PREFIX + "add")) {
+                Optional<Role> optionalRole = member.getRoles().stream().filter(role -> role.hasPermission(Permission.ADMINISTRATOR)).findFirst();
+                boolean isAdmin = optionalRole.isPresent();
+                if (isAdmin) {
+                    long guildId = guild.getIdLong();
+                    long channelId = textChannel.getIdLong();
+                    EntityManager entityManager = factory.createEntityManager();
+                    try {
+                        GuildInfo singleResult = entityManager
+                                .createQuery("SELECT g FROM GuildInfo AS g WHERE g.guildId=?1 and g.guildChannelId=?2", GuildInfo.class)
+                                .setParameter(1, guildId)
+                                .setParameter(2, channelId)
+                                .setMaxResults(1)
+                                .getSingleResult();
+                        if (singleResult != null) {
+                            throw LOGGER.throwing(new AlreadyExistsException("Canal já foi adicionado anteriormente"));
+                        }
+                    } catch (NoResultException e) {
+                        LOGGER.trace(String.format("Nenhum canal foi encontrado, cadastrando canal '%d' do servidor '%d' no banco de dados", channelId, guildId), e);
+                        GuildInfo guildInfo = new GuildInfo(guildId, channelId);
+                        entityManager.getTransaction().begin();
+                        entityManager.persist(guildInfo);
+                        entityManager.getTransaction().commit();
+                        entityManager.close();
+                        msg.reply("Configurado com sucesso").queue(deleteMessagesAfterTime);
+                    } catch (AlreadyExistsException e) {
+                        LOGGER.debug(e);
+                        msg.reply(e.getMessage()).queue(deleteMessagesAfterTime);
+                    } catch (Exception e) {
+                        LOGGER.warn(e);
+                        msg.reply("Ocorreu uma falha ao adicionar o canal").queue(deleteMessagesAfterTime);
+                    }
+                } else {
+                    msg.reply("Você não é um administrador para realizar essa ação").queue(deleteMessagesAfterTime);
+                }
+            } else if (content.equals(COMMAND_PREFIX + "remove")) {
+                boolean isAdmin = member.getRoles().stream().anyMatch(role -> role.hasPermission(Permission.ADMINISTRATOR));
+                if (isAdmin) {
+                    long guildId = guild.getIdLong();
+                    long channelId = textChannel.getIdLong();
+                    EntityManager entityManager = factory.createEntityManager();
+                    try {
+                        GuildInfo singleResult = entityManager
+                                .createQuery("SELECT g FROM GuildInfo AS g WHERE g.guildId=?1 and g.guildChannelId=?2", GuildInfo.class)
+                                .setParameter(1, guildId)
+                                .setParameter(2, channelId)
+                                .setMaxResults(1)
+                                .getSingleResult();
+
+                        entityManager.getTransaction().begin();
+                        entityManager.remove(singleResult);
+                        entityManager.getTransaction().commit();
+                        entityManager.close();
+                        msg.reply("Canal removido com sucesso").queue(deleteMessagesAfterTime);
+                    } catch (NoResultException e) {
+                        LOGGER.debug(e);
+                        msg.reply("Parece que esse canal não está cadastrado para poder ser removido").queue(deleteMessagesAfterTime);
+                    } catch (Exception e) {
+                        LOGGER.warn(e);
+                        msg.reply("Ocorreu uma falha ao remover o canal").queue(deleteMessagesAfterTime);
+                    }
+                } else {
+                    msg.reply("Você não é um administrador para poder realizar essa ação").queue(deleteMessagesAfterTime);
+                }
             }
         }
     }
--- src/main/java/com/ferroeduardo/webcord/listener/ReadyListener.java
+++ src/main/java/com/ferroeduardo/webcord/listener/ReadyListener.java
@@ -1,5 +1,6 @@
 package com.ferroeduardo.webcord.listener;
 
+import com.ferroeduardo.webcord.Util;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
@@ -14,10 +15,7 @@ public class ReadyListener implements EventListener {
     @Override
     public void onEvent(@NotNull GenericEvent event) {
         if (event instanceof ReadyEvent) {
-            long clientId = event.getJDA().getSelfUser().getIdLong();
-            int permission = 2048;
-            String link = String.format("https://discord.com/api/oauth2/authorize?client_id=%d&permissions=%d", clientId, permission) + "&scope=bot%20applications.commands";
-            LOGGER.info("API is ready! Invite link: " + link);
+            LOGGER.info("API is ready! Invite link: " + Util.getInviteLink(event.getJDA()));
         }
     }
 
